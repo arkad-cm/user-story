392ff3113a921a880a8c918b20bd43fa6c94bc00
arkadevbanerjee <arkadev220701@gmail.com>
2021-12-20 23:07:01 +0530

    Complete Assignment 2 - (Part 1 + 2)

---
+++ README.md
@@ -0,0 +1,7 @@
+#user-story
+
+### Instructions
+* npm install
+* npm start
+* Send a POST Request to / with a file with fieldname `'file'`
+* Get back a file with the parsed output
---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', indexRouter);
+
+app.listen(8000, () => console.log("Server is listening at port 8000"))
+
+module.exports = app;
